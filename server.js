e2e99d9c3a7c353fd99f612e98872a3841a45384
californiabones <frefferob@gmail.com>
2026-02-26 15:01:14 +1100

    i dont really know the file isn't working

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(`<h1>Hello, the current time is ${new Date().toLocaleTimeString()}
-  !</h1>`);
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  if (req.method === "GET" && parsedUrl.
+    pathname === "/get-form") {
+      const name = parsedUrl.searchParams.get("name") || "Guest";
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`<h1>Hello, ${name}! This data was sent using GET.</h1>`);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+    }
 });
 
 server.listen(3005, () => console.log("Server running on http://localhost:3005"));
